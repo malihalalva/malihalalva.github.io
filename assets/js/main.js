3e2ed60e27d7ac3e813c6008f8d13698ffc5fc68
malihalalva <malihalalva@users.noreply.github.com>
2024-03-22 01:04:16 -0400

    Inital Commit

---
+++ assets/js/main.js
@@ -0,0 +1,386 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+     navToggle = document.getElementById('nav-toggle'),
+     navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el)  =>{
+    el.addEventListener('click', toggleSkills)
+
+})  
+
+//Ind Skills in Mechanical Skills Section
+const skillsItem = document.getElementsByClassName('skills__item'),
+      skillsName = document.querySelectorAll('.indSkill__Header')
+
+function toggleIndSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsItem.length; i++){
+        skillsItem[i].className = 'skills__item skillsInd__close';
+    }
+
+    if(itemClass === 'skills__item skillsInd__close'){
+        this.parentNode.className = 'skills__item skillsInd__open';
+    }
+
+}
+
+skillsName.forEach((el)  =>{
+    el.addEventListener('click', toggleIndSkills)
+
+})  
+
+/* function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el)  =>{
+    el.addEventListener('click', toggleSkills)
+})
+
+function toggleSkills(clickedArrow) {
+    const skillsContent = clickedArrow.closest('.skills__content');
+    skillsContent.classList.toggle('skills__open');
+    skillsContent.classList.toggle('skills__close');
+    clickedArrow.classList.toggle('skills__arrow--rotate');
+}
+
+function toggleDescription(event, clickedArrow) {
+    console.log(clickedArrow.classList)
+    event.stopPropagation(); // Prevent event from bubbling up to the parent
+
+    const skillItem = clickedArrow.closest('.skills__item');
+    const description = skillItem.querySelector('.skills__description');
+    const link = skillItem.querySelector('.skills__link');
+
+    // Toggle visibility
+    description.style.display = description.style.display === 'none' ? 'block' : 'none';
+    link.style.display = link.style.display === 'none' ? 'inline-block' : 'none';
+
+    // Ensure this toggles the class on the arrow icon itself
+    clickedArrow.classList.toggle('skills__arrow--rotate');
+}
+*/
+
+
+/*==== Expand Button =====*/
+
+var btnDescClick = false;
+
+function ShowDesc() {
+    if (!btnDescClick) {
+        for(i = 0; i < skillsItem.length; i++){
+            skillsItem[i].className = 'skills__item skillsInd__open';
+        }
+
+        btnDescClick = true;
+
+        document.querySelector('#btnExpandCollapse').value = 'Collapse All';
+    }
+    else {
+        for(i = 0; i < skillsItem.length; i++){
+            skillsItem[i].className = 'skills__item skillsInd__close';
+        }
+
+        btnDescClick = false;
+
+        document.querySelector('#btnExpandCollapse').value = 'Expand All';
+    }
+}
+
+/*==================== Expand button ====================*/
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const allSkillsContents = document.querySelectorAll('.skills__content');
+
+//     const expandAllBtn = document.querySelector('.expand-all-btn');
+//     expandAllBtn.addEventListener('click', () => {
+//         let isAnyOpen = Array.from(allSkillsContents).some(content => content.classList.contains('skills__open'));
+
+//         allSkillsContents.forEach((content) => {
+//             const list = content.querySelector('.skills__list');
+//             const arrow = content.querySelector('.skills__arrow');
+//             if (isAnyOpen) {
+//                 content.classList.remove('skills__open');
+//                 content.classList.add('skills__close');
+//                 list.style.display = 'none';
+//                 arrow.classList.remove('skills__arrow--rotate');
+//             } else {
+//                 content.classList.add('skills__open');
+//                 content.classList.remove('skills__close');
+//                 list.style.display = 'block';
+//                 arrow.classList.add('skills__arrow--rotate');
+//             }
+//         });
+
+//         expandAllBtn.textContent = isAnyOpen ? 'Expand All' : 'Collapse All';
+//         isAnyOpen = !isAnyOpen;
+//     });
+// });
+
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.projects__modal'),
+      modalBtns = document.querySelectorAll('.projects__button'),
+      modalCloses = document.querySelectorAll('.projects__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPaper = new Swiper('.paper__container', {
+    cssMode: true,
+    loop: true,
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+/*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+  });
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[data-id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('data-id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*==================== CONTACT FORM VALIDATIONS ====================*/ 
+var nameError = document.getElementById('name-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+
+function validateName(){
+    var name = document.getElementById('fullName').value;
+
+    if(name.length == 0){
+        nameError.innerHTML = 'Full name is required!';
+        return false;
+    }
+    if( !name.match(/[a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?/) ){
+        nameError.innerHTML = 'Enter full name!';
+        return false;
+    }
+    nameError.innerHTML = '<i class="uil uil-check-circle projects__modal-icon"></i>';
+    return true;
+}
+
+function validateEmail(){
+    var email = document.getElementById('email_id').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'A valid email address is required!';
+        return false;
+    }
+    // if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+    if(!email.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)){
+        emailError.innerHTML = 'Invalid email!';
+        return false;
+    }
+    emailError.innerHTML = '<i class="uil uil-check-circle projects__modal-icon"></i>';
+    return true;
+}
+
+function validateMessage(){
+    var message = document.getElementById('message').value;
+    var required = 30;
+    var left = required - message.length;
+
+    if (left>0){
+        messageError.innerHTML = left + ' more characters are required!';
+        return false;
+    }
+    messageError.innerHTML = '<i class="uil uil-check-circle projects__modal-icon"></i>';
+    return true;
+}
+
+/*==================== EMAIL SERVICE ====================*/ 
+function SendMail(){
+
+    if(!validateName() || !validateEmail() || !validateMessage() ){
+        // alert("Please fix the errors to send a message!");
+        swal("Sorry!", "Please fix the errors to send a message!", "warning");
+        return false;
+    }
+
+    var params = {
+        from_name : document.getElementById("fullName").value,
+        email_id : document.getElementById("email_id").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_4ajtfo5", "template_ryff15a", params).then(function (res){
+        // alert("Success! " + res.status);
+        // alert("Your message has been sent successfully!");
+        swal("Success!", "Your message has been sent!", "success");
+    })
+}
